fdafc7a8848639051c25b3e76e2a018949298d6d
Trevor <Puppacino@Lemonade-Stand.local>
2025-03-20 17:59:36 -0400

    trying something new

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+"use server"
+
 import prisma from "@/lib/db";
-// import { addTask } from "./_actions/auth";
+import { addTask } from "./_actions/auth";
 
 type Task = {
   id: string;
@@ -10,16 +12,9 @@ export default async function Home() {
 
   const tasks = await prisma.task.findMany()
 
-  async function addTask(formData: FormData) {
-    await prisma.task.create({
-      data: {
-        title: formData.get('title') as string,
-      }
-    })
-  }
 
   return <div className="bg-zinc-200 flex min-h-screen flex-col items-center pt-10">
-    <h1 className="text-3xl font-medium">All Tasks:</h1>
+    <h1 className="text-3xl font-medium">All Tasks: </h1>
     <ul>
       {tasks.map((task: Task) => (<li key={task.id}>{task.title}</li>))}
     </ul>
@@ -28,7 +23,6 @@ export default async function Home() {
       <input type="text" name="title" className="px-3 py-1 rounded " />
       <button type="submit" className="bg-blue-500 text-white px-3 py-1 mt-3 rounded">Add Task</button>
     </form>
-
 
   </div>;
 }
