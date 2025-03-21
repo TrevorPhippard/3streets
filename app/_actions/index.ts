759c6a7054f7b8475e916fd6e78da4b430d5854e
Trevor <Puppacino@Lemonade-Stand.local>
2025-03-20 23:31:16 -0400

    new direction

---
+++ app/_actions/index.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import prisma from '../../lib/db';
+import { revalidatePath } from "next/cache";
+
+export async function createTask(formData: FormData) {
+    const input = formData.get("title") as string;
+    console.log(input)
+
+    await prisma.task.create({
+        data: {
+            title: input,
+        },
+    });
+    revalidatePath("/");
+}
+
+
+export async function getData() {
+    return await prisma.task.findMany({
+      select: {
+        title: true,
+        id: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
+  
